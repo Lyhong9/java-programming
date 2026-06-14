5e4150a3fca654bf45ca496e55749d980f99f482
Lyhong9 <activatorcode9999@gmail.com>
2026-06-14 23:44:24 +0700

    Apply encapsulation to Guessing Game

--- src/month02/projects/guessGame/GuestGame.java
+++ src/month02/projects/guessGame/GuestGame.java
@@ -20,21 +20,23 @@ public class GuestGame {
         Player player3 = new Player();
 
         while (true) {
+            System.out.println("Please enter your number only 0-9");
+
             System.out.print("Lyhong's number is: ");
-            player1.playerNumber = input.nextInt();
+            player1.setPlayerNumber(input.nextInt());
             System.out.print("Vathana's number is: ");
-            player2.playerNumber = input.nextInt();
+            player2.setPlayerNumber(input.nextInt());
             System.out.print("Phasuk's number is: ");
-            player3.playerNumber = input.nextInt();
+            player3.setPlayerNumber(input.nextInt());
             int randomNumber = randomNumber();
 
-            if (player1.playerNumber == randomNumber) {
+            if (player1.getPlayerNumber() == randomNumber) {
                 System.out.println("Lyhong won the guessing game: " + randomNumber);
                 break;
-            } else if (player2.playerNumber == randomNumber) {
+            } else if (player2.getPlayerNumber() == randomNumber) {
                 System.out.println("Vathana won the guessing game: " + randomNumber);
                 break;
-            } else if (player3.playerNumber == randomNumber) {
+            } else if (player3.getPlayerNumber() == randomNumber) {
                 System.out.println("Phasuk won the guessing game: " + randomNumber);
                 break;
             } else {
--- src/month02/projects/guessGame/Player.java
+++ src/month02/projects/guessGame/Player.java
@@ -1,5 +1,17 @@
 package month02.projects.guessGame;
 
 public class Player {
-    int playerNumber;
+    private int playerNumber;
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPlayerNumber(int playerNumber) {
+        if (playerNumber >= 0 && playerNumber < 10) {
+            this.playerNumber = playerNumber;
+        }else  {
+            System.out.println("Invalid player number!");
+        }
+    }
 }
